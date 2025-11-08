5024f5628387e8aae8f6307b4157ea656c11fba7
Raj <codingfreak202527@gmail.com>
2025-11-08 09:57:43 +0530

    Create StudentServlet for handling student data

---
+++ Studentservlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        // Database saving code
+    }
+}
